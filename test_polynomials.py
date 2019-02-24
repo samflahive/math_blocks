7344041d2d676103569fb8e162b34e2dfe85537a
samflahive <sam.flahive@ucdconnect.ie>
2019-02-24 02:04:14 +0000

    added basic test for add and mult polynomials - using evaluation

--- test_polynomials.py
+++ test_polynomials.py
@@ -11,7 +11,7 @@ import cubics
 class test_poly(unittest.TestCase):
 
     def test_latex(self):
-        # test that polynomials are formatted correctly in latex
+        # test that polynomials are formatted correctly in latex and evaluation
         x = variables.variable("x")
         y = variables.variable("y")
 
@@ -34,31 +34,36 @@ class test_poly(unittest.TestCase):
         latex_format = poly.latex()
         desired_latex = "xy^2+y^3x^2+4x^3y^3-3x^2y^2-xy"
         self.assertEqual(latex_format, desired_latex)
-
-    def test_poly_eval(self):
-        # test polynomials are created accuaretly by evaluating them
-        x = variables.variable("x")
-        y = variables.variable("y")
-
-        x1 = exponentials.exponential(x,1)
-        x2 = exponentials.exponential(x,2)
-        x3 = exponentials.exponential(x,3)
-
-        y1 = exponentials.exponential(y,1)
-        y2 = exponentials.exponential(y,2)
-        y3 = exponentials.exponential(y,3)
-
-        coeffs = [1,1,4,-3,-1]
-        terms = [[x1,y2],
-                 poly_terms.poly_term(y3,x2),
-                 poly_terms.poly_term(x3,y3),
-                 [x2,y2],
-                 poly_terms.poly_term(x1,y1)]
-
-        poly = polynomials.polynomial(coeffs, terms)
         x.value = 3
         y.value = 5
         self.assertEqual(poly.evaluate(), 14010)
+
+    def test_ops(self):
+        # test the + * and == operators between polynomials
+        
+        x = variables.variable("x")
+        g = variables.variable("g")
+        
+        quad = quadratics.quadratic(coeffs=[1,2,3], variable=x) # 11
+        cub = cubics.cubic(coeffs=[1,2,3,4], variable=x) # 26
+
+        # add them
+        quad_cub_add = quad+cub # 11+26 = 37
+        # multiply them
+        quad_cub_mul = quad*cub # 11*26 = 286
+        
+        quad_2 = quadratics.quadratic(coeffs=[1,1,1], variable=g) # 31
+
+        final_poly = (quad_cub_mul+quad_cub_add)*quad_2 # (286+37)*31 = 10013
+
+        # == not finsihed yet - TO DO
+        # reduce not finished -> cant test latex or attributes - TO DO
+
+        # test the operations via evaluation
+        x.value = 2
+        g.value = 5
+        final_poly_value = 10013
+        self.assertEqual(final_poly.evaluate(), final_poly_value)
 
 
     def test_quad_init(self):
