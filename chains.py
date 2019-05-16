552526f3b76e82022b01c44567fb3cfd0a15bb30
samflahive <sam.flahive@ucdconnect.ie>
2019-05-16 18:44:15 +0100

    added basic chains and fractions, only latex and evaluate for now

---
+++ chains.py
@@ -0,0 +1,67 @@
+import number_formatting
+
+class chain:
+    # sums of math_block objects
+
+    def __init__(self, adders, subbers, order):
+        # adders are objects to be added in the chain: list of math_blocks objects
+        self.adders = adders
+        # subbers are objects to be subtracted in the chain: list of math_blocks objects
+        self.subbers = subbers
+        # order is the order in which the adders and subbers should be mixed when displayed: [[adders indices],[subbers indices]]
+        self.order = order
+
+    def evaluate(self):
+        """
+        evaluate this chains
+        return number
+        """
+        return sum(item.evaluate() for item in self.adders) - sum(item.evaluate() for item in self.subbers)
+
+    
+
+    def latex(self, explicit=False):
+        """
+        latex string representing the chain
+        """
+        latex = ""
+        
+        adder_order_index = 0
+        subber_order_index = 0
+
+        # loop through the order until the final one of each is reached
+        while adder_order_index < len(self.order[0]) or subber_order_index < len(self.order[1]):
+            # might have to block some functionality if the adders are finished before the subbers or vice versa
+            adder_finished = (adder_order_index >= len(self.order[0]))
+            subber_finished = (subber_order_index >= len(self.order[1]))
+            print(adder_order_index, subber_order_index)
+            
+            
+            # the print order of the current adder and subber - if not finished
+            adder_index = self.order[1][subber_order_index]+1 if adder_finished else self.order[0][adder_order_index]
+            subber_index = self.order[0][adder_order_index]+1 if subber_finished else self.order[1][subber_order_index]
+
+            # the current adder is before the current subber
+            if adder_index < subber_index:
+                adder = self.adders[adder_order_index]
+                if isinstance(adder, (int, float, complex)):
+                    latex += number_formatting.number_coeff(adder, adder_order_index, explicit=explicit, one_special=False)
+                else:
+                    latex += "{}{}".format("+" if adder_order_index != 0 else "",adder.latex(explicit=explicit))
+
+                adder_order_index += 1
+
+            # the current subber is before the current adder
+            else:
+                subber = self.subbers[subber_order_index]
+                if isinstance(adder, (int, float, complex)):
+                    latex += number_formatting.number_coeff(-subber, subber_order_index, explicit=explicit, one_special=False)
+                else:
+                    latex += "{}{}".format("-" if subber_order_index != 0 else "",subber.latex(explicit=explicit))
+
+                subber_order_index += 1
+
+        return latex
+
+                
+        
---
+++ fractions.py
@@ -0,0 +1,33 @@
+import number_formatting
+
+class fraction:
+    def __init__(self, num, den):
+        # num is the fraction numerator
+        # den is the fraction denominator
+        self.numerator = num
+        self.denominator = den
+
+    def evaluate(self):
+        """
+        calculate the value of this fraction at current variable values
+        return number
+        """
+        num_val = self.numerator if isinstance(self.numerator, (int, float, complex)) else self.numerator.evaluate()
+        den_val = self.denominator if isinstance(self.denominator, (int, float, complex)) else self.denominator.evaluate()
+
+        return num_val/den_val
+
+    def latex(self, explicit=False):
+        """
+        return the latex format of this fraction
+        """
+        if isinstance(self.numerator, (int, float, complex)):
+            num_latex = number_formatting.number_coeff(self.numerator , 0, explicit=explicit, one_special=True)
+        else:
+            num_latex = self.numerator.latex(explicit=explicit)
+        if isinstance(self.denominator, (int, float, complex)):
+            den_latex = number_formatting.number_coeff(self.denominator , 0, explicit=explicit, one_special=True)
+        else:
+            den_latex = self.denominator.latex(explicit=explicit)
+
+        return r"\frac{{{0}}}{{{1}}}".format(num_latex, den_latex)
