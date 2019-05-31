c08102a215db8c797598c6f6db7a9d07d6db7150
Sam Flahive <sam.flahive@ucdconnect.ie>
2019-05-31 23:45:37 +0200

    added __mul__ to variable

--- variables.py
+++ variables.py
@@ -1,8 +1,11 @@
 class variable:
     def __init__(self, symbol):
         self.symbol = symbol
+        self.sign = True
 
     def __eq__(self, other):
+        if not isinstance(other, variable):
+            return False
         return self.symbol == other.symbol
 
     def set_value(self, value):
