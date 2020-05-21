deee6b1b84d61b425aec18734aea58c1ede6aef6
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-05-21 03:00:39 +0200

    restructured chains to take list of tuples (value, sign)

--- math_blocks/chains.py
+++ math_blocks/chains.py
@@ -6,7 +6,9 @@ class chain(math_block):
     # sums of math_block objects
 
     def __init__(self, items, sign=True):
-        # adders are objects to be added in the chain: list of math_blocks objects
+        """chains or simple sums, items refers to a list of tuples.
+            The first value in each tuple is a number or math_block object,
+            the second value is a sign, True=+ and False=- """
         math_block.__init__(self, sign=sign)
         self.items = items
 
@@ -15,14 +17,21 @@ class chain(math_block):
         evaluate this chains
         return number
         """
-        return sum(list(map(lambda x: x if isinstance(x,(int,float,complex)) else x.evaluate(), self.items)))
+        total = 0
+        for (item, sign) in self.items:
+            item_value = item if isinstance((int, float)) else item.evaluate()
+            if not sign:
+                total -= item_value
+            else:
+                total += item_value
+        return total
 
     def __add__(self, other):
         """
         addition operator for chain objects - supports all non-chain objects
         return chain object
         """
-        return chain(items=[*self.items, other], sign=self.sign)
+        return chain(items=[*self.items, (other, True)], sign=self.sign)
 
     def __sub__(self, other):
         other = -other
@@ -36,14 +45,18 @@ class chain(math_block):
         """
         latex_list = []
         
-        for index,term in enumerate(self.items):
-            if isinstance(term, (int, float)):
-                latex = number_coeff(term, index=index)
-
+        for index, (item, sign) in enumerate(self.items):
+            if isinstance(item, (int, float)):
+                    item_latex = item
+                    
             else:
-                latex = term.latex(show_plus=(index != 0))
-
-            latex_list.append(latex)
-                
-            out = "".join(latex_list)
+                item_latex = item.latex(explicit=explicit)
+                if not (index == 0 and not explicit and sign):
+                    sign_sym = "+" if sign else "-"
+                    if not item.sign:
+                        item_latex = "%s(%s)" % (item_latex, sign_sym)
+                    else:
+                        item_latex = "%s%s" % (item_latex, sign_sym)
+            latex_list.append(item_latex)
+        out = "".join(latex_list)
         return out if self.sign else "-({})".format(out)
