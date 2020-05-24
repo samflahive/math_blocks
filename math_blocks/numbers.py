67bb08c47b3586fe1ecb56fa587f82658b28195a
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-05-24 12:23:23 +0200

    fixed bug: negative sign now used in eval

--- math_blocks/numbers.py
+++ math_blocks/numbers.py
@@ -2,16 +2,17 @@ from .math_block import math_block
 
 class number(math_block):
 
-         def __init__(value, sign=True):
+         def __init__(self, value, sign=True):
                   implied_sign = value > 0
-                  math_block.__init__(self, sign=(implied_sign and sign))
+                  math_block.__init__(self, sign=(implied_sign == sign))
 
                   value = abs(value)
                   self.value = value
 
          def latex(self, explicit=False):
-                  sign = "+" if self.sign else "-"
-                  return "{}{}".format(sign, self.value)
+                  if not self.sign:
+                           return "-{}".format(self.value)
+                  return str(self.value)
 
          def evaluate(self):
                   if self.sign:
