9939e42885fab2b4c93db2c83520770743dd9161
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-05-24 12:21:26 +0200

    reduced scope of exponentials (inti, eval, latex). Replace int and float inputs with mathblock numbers, and wrap bases in () if required

--- math_blocks/exponentials.py
+++ math_blocks/exponentials.py
@@ -1,61 +1,50 @@
 from .math_block import math_block
+from .numbers import number
+from .variables import variable
 import math
 import copy
 
 class exponential(math_block):
      
-     def __init__(self, base, power):
-         self.base = base
-         self.power = power
-         math_block.__init__(self, self.sign)
-         
-     def __mul__(self, other):
-          if isinstance(other, exponential):
-               if self.base == other.base:
-                    return exponential(self.base, self.power+other.power)
+     def __init__(self, base, power, sign=True):
+          math_block.__init__(self, sign=sign)
 
-               same_base = exponential.change_base(other, self.base)
-               return exponential(self.base, self.power+same_base.power)
+          # replace python numbers with mathblock numbers
+          if isinstance(base, (int, float)):
+               base = number(base)
+          if isinstance(power, (int, float)):
+               power = number(power)
                
-          elif isinstance(other, (int, float)):
-               return product([self, other])
-          else:
-               return NotImplemented
-     
+          self.base = base
+          self.power = power
 
-     def __add__(self, other):
-          pass
           
      
      def evaluate(self):
          # extract the value of the base whether its a number or variable
-         base_value = self.base if isinstance(self.base, (int, float, complex)) else self.base.evaluate()
-         power_value = self.power if isinstance(self.power, (int, float, complex)) else self.power.evaluate()
+         base_value = self.base.evaluate()
+         power_value = self.power.evaluate()
          val = base_value**power_value
          return val if self.sign else -val
 
 
-     def latex(self, explicit=False):
-          # extract the value of the base whether its a number or variable
-          if isinstance(self.base, (int, float)):
-               base_symbol = self.base
-          else:
-               lat = self.base.latex()
+     def latex(self, explicit=False, show_zero_power=True):
+          base_symbol = self.base.latex(explicit=explicit)
+          if not self.base.sign or not isinstance(self.base, (number, variable)):
+               base_symbol = "(%s)" % base_symbol
                
-          power_symbol = self.power if isinstance(self.power, (int, float)) else self.power.latex()
+          power_symbol = self.power.latex(explicit=explicit)
           
           if explicit:
                out = "{}^{{{}}}".format(base_symbol, power_symbol)
           else:
                if self.power == 0:
-                    out = ""
+                    out = "1" if show_zero_power else ""
                elif self.power == 1:
                     out = "{}".format(base_symbol)
                else:
                     out = "{}^{{{}}}".format(base_symbol, power_symbol)
 
-          out = object_sign(show_plus=show_plus, sign=self.sign)+out
-          
           return out if self.sign else "-%s" % out
 
 
--- tests/exponentials.py
+++ tests/exponentials.py
@@ -3,6 +3,7 @@ import unittest
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 from math_blocks.exponentials import exponential
+from math_blocks.variables import variable
 
 
 class basic_exponential(unittest.TestCase):
@@ -12,39 +13,39 @@ class basic_exponential(unittest.TestCase):
         basic_exp_B = exponential(4,2)
         self.assertEqual(basic_exp_B.latex(), "4^{2}")
 
+    def test_variable_latex(self):
+        x = variable("x")
+        basic_exp_A = exponential(2,x, sign=False)
+        self.assertEqual(basic_exp_A.latex(), "-2^{x}")
+
+        y = variable("y", sign=False)
+        basic_exp_B = exponential(y,2)
+        self.assertEqual(basic_exp_B.latex(), "(-y)^{2}")
+
+    def test_exponential_latex(self):
+        exp1 = exponential(2,3)
+        exp2 = exponential(exp1, 2)
+        exp3 = exponential(exp1, exp2)
+
+        self.assertEqual(exp3.latex(), "(2^{3})^{(2^{3})^{2}}")
+
     def test_basic_evaluation(self):
         basic_exp_A = exponential(2,-3)
         self.assertEqual(basic_exp_A.evaluate(), 2**-3)
         basic_exp_B = exponential(4,2)
         self.assertEqual(basic_exp_B.evaluate(), 4**2)
 
-    def test_basic_operations(self):
-        # multiplying exponentials (same base)
-        basic_exp_A = exponential(2,-3)
-        basic_exp_B = exponential(2,2)
-        basic_exp_C = basic_exp_A*basic_exp_B
-        self.assertEqual(basic_exp_C.evaluate(), 2**-1)
-        self.assertEqual(basic_exp_C.latex(), "2^{-1}")
+    def test_variable_evaluation(self):
+        x = variable("x")
+        x.value = 3
+        basic_exp_A = exponential(2,x, sign=False)
+        self.assertEqual(basic_exp_A.evaluate(), -(2**3))
 
-        # dividing exponentials (same base)
-        basic_exp_D = basic_exp_B / basic_exp_A
-        self.assertEqual(basic_exp_D.evaluate(), 2**5)
-        self.assertEqual(basic_exp_D.latex(), "2^{5}")
+        y = variable("y", sign=False)
+        y.value = 2
+        basic_exp_B = exponential(y,3)
+        self.assertEqual(basic_exp_B.evaluate(), (-2)**3)
 
-        # adding and subtracting exponentials
-        exp_sum = basic_exp_A + basic_exp_B - basic_exp_C
-        self.assertEqual(exp_sum.latex(), "2^{-3}+4^{8}-2^{-1}")
-        self.assertEqual(exp_sum.evaluate(), 2**-3 + 4**2 - 2**-1)
-
-        # multiplying exponential and number
-        exp_prod = basic_exp_D * 2
-        self.assertEqual(exp_prod.evaluate(), 2**6)
-        self.assertEqual(exp_prod.latex(), "2^{5} \cdot 2")
-
-        # dividing exponential and number
-        exp_frac = basic_exp_D / 3
-        self.assertEqual(exp_frac.latex(), "\frac{2^{5}}{3}")
-        self.assertEqual(exp_frac.evaluate(), 2**5 / 3)
 
         
 if __name__ == '__main__':
