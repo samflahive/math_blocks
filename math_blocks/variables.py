664212e904ca9289aee8b1bb0a42e49a043436f0
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-05-24 12:22:26 +0200

    convert variable values to mathblock

--- math_blocks/variables.py
+++ math_blocks/variables.py
@@ -1,16 +1,27 @@
 from .math_block import math_block
-
+from .numbers import number
 
 class variable(math_block):
-    def __init__(self, symbol, sign=True):
+    def __init__(self, symbol, sign=True, value=None):
         math_block.__init__(self, sign=sign)
         self.symbol = symbol
         self.sign = sign
+        if isinstance(value, (int, float)):
+            value = number(value)
+        self.value = value
 
     def latex(self, explicit=False):
-        if self.sign:
+        if not self.sign:
             return "-%s" % self.symbol
         return self.symbol
     
     def evaluate(self):
-        return self.value if self.sign else -self.value
+        if self.value != None:
+            if isinstance(self.value, (int, float)):
+                self.value = number(self.value)
+            val = self.value.evaluate()
+            if self.sign:
+                return val
+            return -val
+        else:
+            raise ValueError("variable (symbol=%s) does not have a value" % self.symbol)
--- tests/variables.py
+++ tests/variables.py
@@ -3,6 +3,7 @@ import unittest
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 from math_blocks.variables import variable
+from math_blocks.exponentials import exponential
 
 
 class basic_variable(unittest.TestCase):
@@ -26,6 +27,16 @@ class basic_variable(unittest.TestCase):
                   n3 = variable("z", sign=False)
                   # no value set -> error
                   self.assertRaises(ValueError, n3.evaluate)
+
+         def test_exponential_eval(self):
+                  n = variable("x")
+                  n.value = exponential(2,3)
+                  self.assertEqual(n.evaluate(), 2**3)
+
+                  n2 = variable("y", sign=False)
+                  n2.value = exponential(2,3, sign=False)
+                  self.assertEqual(n2.evaluate(), 2**3)
+
 
 
     
