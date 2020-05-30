55f7bbf603d0c9696da2ad20a4bafae43c8e5e64
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-05-31 01:03:35 +0200

    created default __add__ that returns a chain of the operands

--- math_blocks/math_block.py
+++ math_blocks/math_block.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-
+import math_blocks
 class math_block:
     def __init__(self, sign, product_bracket=False,
                   chain_bracket=False, bracketed=True):
@@ -7,6 +7,16 @@ class math_block:
         self.product_bracket = product_bracket
         self.chain_bracket = chain_bracket
         self.bracketed = bracketed
+
+
+    def __add__(self, other):
+        return math_blocks.chain([self, other])
+
+    def __mul__(self, other):
+        return math_blocks.product([self, other])
+
+    def __truediv__(self, other):
+        return math_blocks.fraction(self, other)
               
     def __neg__(self):
         new = deepcopy(self)
