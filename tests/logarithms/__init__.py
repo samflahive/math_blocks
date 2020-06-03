5a9d9eb4a14a4de85557e826579ff0d4649f2c10
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-06-03 19:43:44 +0200

    tunred directory into a package£

---
+++ tests/logarithms/__init__.py
@@ -0,0 +1,6 @@
+import unittest
+from creation.logarithms import *
+
+if __name__ == '__main__':
+    unittest.main()
+
--- tests/logarithms/creation/logarithms.py
+++ tests/logarithms/creation/logarithms.py
@@ -1,13 +1,13 @@
 import unittest
 
-from basic_logarithms import basic_logarithm_test
-from variable_logarithms import variable_logarithm_test
-from fraction_logarithms import fraction_logarithm_test
-from exponential_logarithms import exponential_logarithm_test
-from complex_number_logarithms import complex_logarithm_test
-from logarithm_logarithms import logarithm_logarithm_test
-from product_logarithms import product_logarithm_test
-from chain_logarithms import chain_logarithm_test
+from .basic_logarithms import basic_logarithm_test
+from .variable_logarithms import variable_logarithm_test
+from .fraction_logarithms import fraction_logarithm_test
+from .exponential_logarithms import exponential_logarithm_test
+from .complex_number_logarithms import complex_logarithm_test
+from .logarithm_logarithms import logarithm_logarithm_test
+from .product_logarithms import product_logarithm_test
+from .chain_logarithms import chain_logarithm_test
 
 
 if __name__ == '__main__':
