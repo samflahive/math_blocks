0c1c0e0e297f844a7c920d9337ee60ccbcf5db9c
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-06-04 15:09:27 +0200

    master __init__ for all tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,15 @@
 import unittest
 
 
-from .numbers import *
-
+from chains import *
+from complex_numbers import *
+from exponentials import *
+from fractions import *
+from logarithms import *
+from numbers import *
+from polynomials import *
+from products import *
+from variables import *
 
 if __name__ == '__main__':
     unittest.main()
