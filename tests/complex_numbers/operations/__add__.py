74d3cf1e347b91325d4b51bfcf745b1125206821
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-06-06 21:18:16 +0200

    added __add__ tests

--- tests/complex_numbers/operations/__add__.py
+++ tests/complex_numbers/operations/__add__.py
@@ -7,23 +7,23 @@ class complex_add(unittest.TestCase):
 
     
     def test_number_add(self):
-        main_cx = math_blocks.complex(2,3)
+        main_cx = math_blocks.complex_number(2,3)
         n = math_blocks.number(3)
 
         result = main_cx + n
         
-        result_latex = "(2+3j)+3"
+        result_latex = "2+3i+3"
         result_value = complex(5,3)
         self.assertEqual(result.latex(), result_latex)
         self.assertEqual(result.evaluate(), result_value)
 
     def test_variable_add(self):
-        main_cx = math_blocks.complex(2,3)
+        main_cx = math_blocks.complex_number(2,3)
         v = math_blocks.variable("v", value=3)
 
         result = main_cx + v
         
-        result_latex = "(2+3j)+v"
+        result_latex = "2+3i+v"
         result_value = complex(5,3)
         
         self.assertEqual(result.latex(), result_latex)
@@ -31,12 +31,12 @@ class complex_add(unittest.TestCase):
 
 
     def test_product_add(self):
-        main_cx = math_blocks.complex(2,3)
+        main_cx = math_blocks.complex_number(2,3)
         p = math_blocks.product([1,2,3])
 
         result = main_cx + p
         
-        result_latex = "(2+3j)+(1 \cdot 2 \cdot 3)"
+        result_latex = "2+3i+(1 \cdot 2 \cdot 3)"
         result_value = complex(8,3)
         
         self.assertEqual(result.latex(), result_latex)
@@ -44,37 +44,37 @@ class complex_add(unittest.TestCase):
 
 
     def test_poly_eq(self):
-        main_cx = math_blocks.complex(2,3)
+        main_cx = math_blocks.complex_number(2,3)
         a = math_blocks.variable("a", value=1)
         p = math_blocks.simple_poly([1,2,3], a)
 
         result = main_cx + p
         
-        result_latex = "(2+3j)+(1a^{2}+2a+3)"
+        result_latex = "2+3i+(1a^{2}+2a+3)"
         result_value = complex(8,3)
         
         self.assertEqual(result.latex(), result_latex)
         self.assertEqual(result.evaluate(), result_value)
 
     def test_chain_eq(self):
-        main_cx = math_blocks.complex(2,3)
+        main_cx = math_blocks.complex_number(2,3)
         alt_chain_b = math_blocks.chain([1,3,2])
 
         result = main_cx + alt_chain_b
         
-        result_latex = "(2+3j)+(1+3+2)"
+        result_latex = "2+3i+(1+3+2)"
         result_value = complex(8,3)
         
         self.assertEqual(result.latex(), result_latex)
         self.assertEqual(result.evaluate(), result_value)
 
     def test_complex_eq(self):
-        main_cx = math_blocks.complex(2,3)
-        a = math_blocks.complex(2,3)
+        main_cx = math_blocks.complex_number(2,3)
+        a = math_blocks.complex_number(2,3)
 
         result = main_cx + a
         
-        result_latex = "4+6j"
+        result_latex = "4+6i"
         result_value = complex(4,6)
         
         self.assertEqual(result.latex(), result_latex)
@@ -82,7 +82,7 @@ class complex_add(unittest.TestCase):
 
         result = main_cx - a
         
-        result_latex = "0+0j"
+        result_latex = "-0-0i"
         result_value = complex(0,0)
         
         self.assertEqual(result.latex(), result_latex)
