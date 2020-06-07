c17a896f470f8d6f39aca6bbbf89085fc21b1332
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-06-07 12:18:24 +0200

    added commented out from_roots, still needs work

--- math_blocks/simple_polys.py
+++ math_blocks/simple_polys.py
@@ -16,4 +16,37 @@ class simple_poly(polynomial):
 
         polynomial.__init__(self, items=items, sign=sign)
         self.var_sym = var.symbol
+
+"""
+    @staticmethod
+    def root_product_sum(roots, size):
+        if size == 1:
+            return sum(roots)
+        size -= 1
+        total = 0
+        for i in range(len(roots)-size):
+            base = list_product(roots[i:i+size])
+            for j in range(i+size, len(roots)):
+                total += base * roots[j]
+        return total
+
+    
+    @staticmethod
+    def from_roots(var, roots):
+        coeffs = [1]
+        for i in range(len(roots)):
+            coeff  = simple_poly.root_product_sum(roots, i+1)
+            if i % 2 == 0:
+                coeffs.append(-coeff)
+            else:
+                coeffs.append(coeff)
+
+        return simple_poly(coeffs, var)
         
+def list_product(ls):
+    total = 1
+    for i in ls:
+        total *= i
+    return total
+
+"""
