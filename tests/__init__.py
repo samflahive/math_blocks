1abebdf732f481c55d7e06a9d395784138feb92b
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-06-23 21:51:24 +0200

    restructured tests into algebra folder

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,15 +4,7 @@ import unittest
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 
-from chains import *
-from complex_numbers import *
-from exponentials import *
-from fractions import *
-from logarithms import *
-from numbers import *
-from polynomials import *
-from products import *
-from variables import *
+from algebra import *
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/algebra/__init__.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+
+from .chains import *
+from .complex_numbers import *
+from .exponentials import *
+from .fractions import *
+from .logarithms import *
+from .numbers import *
+from .polynomials import *
+from .products import *
+from .variables import *
+
+if __name__ == '__main__':
+    unittest.main()
+
