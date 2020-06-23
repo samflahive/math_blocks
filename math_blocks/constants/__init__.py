1aa23b7d0440777819643700f4ef3e6dc0b659c4
Sam Flahive <sam.flahive@ucdconnect.ie>
2020-06-23 21:38:08 +0200

    e and pi constants

---
+++ math_blocks/constants/__init__.py
@@ -0,0 +1,6 @@
+
+from .constants import Constant
+
+E = Constant(symbol="e", value=2.718281828459045, id="E")
+
+PI = Constant(symbol="\\pi", value=3.141592653589793, id="PI")
---
+++ math_blocks/constants/constants.py
@@ -0,0 +1,27 @@
+from copy import deepcopy
+
+class Constant:
+    def __init__(self, symbol, value, id, sign=True):
+        self.symbol = symbol
+        self.value = value
+        self.id = id
+        self.sign = sign
+
+    def latex(self, explicit=False, show_plus=False):
+        if not self.sign:
+            return f"-{self.symbol}"
+        elif show_plus:
+            return f"+{self.symbol}"
+        else:
+            return self.symbol
+
+    def evaluate(self):
+        if self.sign:
+            return self.value
+        else:
+            return -self.value
+
+    def __neg__(self):
+        copied = deepcopy(self)
+        copied.sign = not self.sign
+        return copied
